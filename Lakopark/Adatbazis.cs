2f5a5bfbe5e0a8290709f5e97e3370a4b3d18c10
anzani-hun <anzani@hun>
2024-01-09 17:14:46 +0100

    first

---
+++ Lakopark/Adatbazis.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Lakopark
+{
+    class Adatbazis
+    {
+        MySqlConnection connection = null;
+        MySqlCommand sql = null;
+
+        public Adatbazis()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Server = "localhost";
+            sb.UserID = "root";
+            sb.Password = "";
+            sb.Database = "lakopark";
+            sb.CharacterSet = "utf8";
+            connection = new MySqlConnection(sb.ConnectionString);
+
+            try
+            {
+                kapcsolatNyit();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            finally
+            {
+                kapcsolatZar();
+            }
+        }
+
+
+        private void kapcsolatZar()
+        {
+            if ( connection.State != System.Data.ConnectionState.Closed )
+            {
+                connection.Close();
+            }
+        }
+
+        private void kapcsolatNyit()
+        {
+            if ( connection.State != System.Data.ConnectionState.Open ) connection.Open();
+        }
+    }
+}
---
+++ Lakopark/Lakopark.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lakopark
+{
+    class Lakopark
+    {
+        readonly int lakoparkId;
+        string lakoparkNeve;
+        int utcakSzama;
+        int telkekSzama;
+
+        int[,] hazak;       //két dimenziós tömb - utca és telek érték alapján az emeletszámot adja vissza.
+
+        public int LakoparkId { get => lakoparkId; }
+        public string LakoparkNeve { get => lakoparkNeve; set => lakoparkNeve = value; }
+        public int UtcakSzama { get => utcakSzama; set => utcakSzama = value; }
+        public int TelkekSzama { get => telkekSzama; set => telkekSzama = value; }
+        public int[,] Hazak { get => hazak; set => hazak = value; }
+
+
+        public Lakopark(int lakoparkId, string lakoparkNeve, int utcakSzama, int telkekSzama)
+        {
+            this.lakoparkId = lakoparkId;
+            LakoparkNeve = lakoparkNeve;
+            UtcakSzama = utcakSzama;
+            TelkekSzama = telkekSzama;
+            this.hazak = new int[utcakSzama, telkekSzama];
+
+        }
+    }
+}
