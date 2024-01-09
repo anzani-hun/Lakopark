1d1f2d2a573215c7adb4ed0068f7edd292d78f75
anzani-hun <anzani@hun>
2024-01-09 16:38:38 +0100

    Add project files.

---
+++ Lakopark/Form_Lakopark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lakopark
+{
+    public partial class Form_Lakopark : Form
+    {
+        public Form_Lakopark()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
